dbd9cb93d971cbb1ecaf2f18ed85062dbeb076a7
anuj-negi-hub <anujnegi9211@gmail.com>
2026-04-29 21:28:41 +0530

    exercise36 - 42 commiting

---
+++ ex37_symbols_review.py
@@ -0,0 +1,6 @@
+# It’s time to review the symbols and Python words you know and pick up a few more for the next few
+# lessons. I have written out all the Python symbols and keywords that are important to know.
+
+# In this lesson take each keyword and first try to write out what it does from memory. Next, search online
+# for it and see what it really does. This may be difficult because some of these are difficult to search for,
+# but try anyway.
