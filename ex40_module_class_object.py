40ef24cf9e46f95f7201ba43ba95ffacd03108ef
anuj-negi-hub <anujnegi9211@gmail.com>
2026-05-03 19:22:11 +0530

    pushing after modules discussion

--- ex40_module_class_object.py
+++ ex40_module_class_object.py
@@ -1,8 +1,11 @@
+# All the python the files 
+# Whenever python file is imported, then entire file will 
 class Song(object):
 
     def __init__(self, lyrics):
         self.lyrics = lyrics
 
+    # This method prints the element in the self.lyrics list / object
     def sing_me_a_song(self):
         for line in self.lyrics:
             print(line)
@@ -14,9 +17,17 @@ happy_bday = Song(["Happy birthday to you",
 bulls_on_parade = Song(["The rally around the family",
                         "with pocket full of shells"])
 
-happy_bday.sing_me_a_song()
-bulls_on_parade.sing_me_a_song()
+string_srong = Song(["This is a song...", 785])
 
+# happy_bday.sing_me_a_song()
+# bulls_on_parade.sing_me_a_song()
+
+
+# when importing this  files as module, the __name__ == "ex40_module_class_object"
+# when executing this  files as module, the __name__ == "__main__"
+print(__name__)
+if __name__ == "__main__":
+    string_srong.sing_me_a_song()
 
 
 
