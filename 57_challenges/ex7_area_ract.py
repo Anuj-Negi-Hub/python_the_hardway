d907d9d1a3112983629618a9900517df87fc7453
anuj-negi-hub <anujnegi9211@gmail.com>
2026-05-06 21:27:29 +0530

    appending the contents to the file

--- 57_challenges/ex7_area_ract.py
+++ 57_challenges/ex7_area_ract.py
@@ -2,6 +2,8 @@
 # Prompt the user for the length and width of the room in feet.
 # Then display the area in both square feet and square meters.
 
+import os
+
 def area(length, width):
     #formula to calculate the area of a ractanguler
     room_area = length * width
@@ -11,12 +13,32 @@ def area(length, width):
     #formula to convert square feet to square meter
     area_meter = room_area * SQFT_TO_SQM
 
-    print(f"The area of the room is {room_area} square feet.")
-    print(f"The area of the room is {area_meter} square meter.")
+    return room_area, area_meter
+
+    # print(f"The area of the room is {room_area} square feet.")
+    # print(f"The area of the room is {area_meter} square meter.")
      
 length = float(input("Enter the length of the room in feet: "))
 width = float(input("Enter the width of the room in feet: "))
 
 print(f"You entered dimensions of {length} feet by {width} feet.")
 
-area(length, width)
+# unpacking
+sqft, sqm = area(length, width)
+
+print(f"The area of the room is {sqft} square feet.")
+print(f"The area of the room is {sqm} square meter.")
+
+# s1 = area(length, width)
+# print(s1)
+
+new_file = open("ex7.txt", "w+")
+
+content_to_paste = new_file.write(
+    f"The area of the room is {sqft} square feet.\n"
+    f"The area of the room is {sqm} square meter."
+    )
+new_file.close()
+
+
+
