21fbf7dabcc189c505d35a0a1fd90bfad4936d85
anuj-negi-hub <anujnegi9211@gmail.com>
2026-05-26 08:51:21 +0530

    Added ex15.py

--- 57_challenges/ex15.py
+++ 57_challenges/ex15.py
@@ -15,4 +15,41 @@ if password == saved_password:
 else:
     print(f"I don't know you! Please type the correct password and try again.")
 
+print()
+#solving the same problem using function
 
+def val_cred(user_name, password):
+
+    saved_password = "User@123"
+
+    if password == saved_password:
+        print(f"Welcome {user_name}!")
+    else:
+        print(f"I don't know you! Please type the correct password and try again.")
+
+user_name = input("Type User Name: ")
+password = input("Type your password: ")
+val_cred(user_name, password)
+
+
+#solve the same problem using class
+
+class Credential():
+    def __init__(self, user_name, password):
+        self.name = user_name
+        self.password = password
+
+    def validate_cred(self):
+        saved_password = "User@123"
+        
+        if self.password == saved_password:
+            print(f"Welcome {self.name}!")
+        else:
+            print(f"I don't know you! Please type the correct password and try again.")
+
+user_name = input("Type User Name: ")
+password = input("Type your password: ")
+
+a1 = Credential(user_name, password)
+
+a1.validate_cred()
